c9be375bcfe0e2897a2e0bdae66a4fc78627fe25
Efe Temel ERSÖZ <80988682+efetemel@users.noreply.github.com>
2021-11-22 18:00:20 +0300

    New feature

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,9 +1,9 @@
 import * as Yup from "yup";
-import {ErrorMessage, Field, Form, Formik} from "formik";
-import {Link} from "react-router-dom";
-import {AuthenticationRequest} from "../dto/AuthenticationRequest";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import  { AuthenticationRequest } from "../dto/AuthenticationRequest";
 import Store from "../redux/Store";
-import {loadUser, loginUser} from "../redux/actions/AuthActions";
+import { loadUser, loginUser } from "../redux/actions/AuthActions";
+import { useState } from "react";
 
 export default function (){
 
@@ -13,7 +13,7 @@ export default function (){
         purchasePrice:Yup.number().min(1).required("Ürün alış fiyatı giriniz"),
         quantity:Yup.number().min(1).required("Ürün stok adedi giriniz"),
         dividend:Yup.number().min(0).required("Ürün kar payı giriniz"),
-        barcode:Yup.number().required("Ürün barkodu giriniz"),
+        barcode:Yup.number().positive("Lütfen geçerli barkod giriniz").required("Ürün barkodu giriniz"),
         description:Yup.string().required("Ürün açıklaması giriniz")
     })
 
@@ -38,13 +38,26 @@ export default function (){
         Store.dispatch(loadUser())
     }
 
+
+    function CheckBarcode(value){
+        if(value.toString().length >= 13){
+            console.log(value,value.toString().length >= 13)
+            return "Barkod kontrol ediliyor"
+        }
+        else{
+            console.log(value,value.toString().length >= 13)
+
+            return "Lütfen geçerli bir barkod giriniz.."
+        }
+    }
+
     return(
         <div>
             <Formik initialValues={initialValues} validationSchema={schema} onSubmit={(values) => HandleAddProduct(values)}>
                 <Form>
                     <div className=" mb-4">
                         <div className="form-group first rounded-3 bg-white border">
-                            <Field type="number" className="form-control" name="barcode" placeholder="Barkod" ></Field>
+                            <Field type="number" className="form-control" name="barcode" placeholder="Barkod" validate={CheckBarcode} ></Field>
                         </div>
                         <ErrorMessage name="barcode" render={(errorMessage) => <p className="mt-2 text-danger">{errorMessage}</p>}></ErrorMessage>
                     </div>
@@ -61,9 +74,8 @@ export default function (){
                         <ErrorMessage name="name" render={(errorMessage) => <p className="mt-2 text-danger">{errorMessage}</p>}></ErrorMessage>
                     </div>
                     <div>
-                        <button type="submit" color="info">Ürünü</button>
+                        <button type="submit" color="info">Ürünü ekle</button>
                     </div>
-                    <p className="text-muted mt-3"><Link to="/forgot-password">Şifremi unuttum</Link></p>
                 </Form>
             </Formik>
         </div>
