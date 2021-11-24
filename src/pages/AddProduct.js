d7d3a9673ef5225f16fd6458b652919570b275c6
Efe Temel ERSÖZ <80988682+efetemel@users.noreply.github.com>
2021-11-24 21:37:30 +0300

    .

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -31,21 +31,34 @@ export default function (){
         categoryName:categories[0].name
     }
 
-    const schema = Yup.object().shape({
-        name:Yup.string().required("Ürün adı giriniz!"),
-        purchasePrice:Yup.number().min(1).required("Ürün alış fiyatı giriniz"),
-        quantity:Yup.number().min(1).required("Ürün stok adedi giriniz"),
-        dividend:Yup.number().min(0).required("Ürün kar payı giriniz"),
-        barcode:Yup.number().required("Ürün barkodu giriniz"),
-        description:Yup.string().required("Ürün açıklaması giriniz"),
-        categoryName:Yup.string().required("Ürün kategorisi giriniz")
-    })
+    const [status,setStatus] = useState();
+
+
+    const [name,setName] = useState();
+    const [nameError,setNameError] = useState();
+
+    const [barcode,setBarcode] = useState();
+    const [barcodeError,setBarcodeError] = useState();
+
+    const [purchasePrice,setPurchasePrice] = useState(1);
+    const [purchasePriceError,setPurchasePriceError] = useState();
+
+    const [quantity,setQuantity] = useState();
+    const [quantityError,setQuantityError] = useState();
 
     const [dividend,setDividend] = useState(0);
-    const [purchasePrice,setPurchasePrice] = useState(0);
+    const [dividendError,setDividendError] = useState();
 
+    const [price,setPrice] = useState();
+    const [priceError,setPriceError] = useState();
 
-    function test(){
+    const [description,setDescription] = useState();
+    const [descriptionError,setDescriptionError] = useState();
+
+    const [category,setCategory] = useState();
+    const [categoryError,setCategoryError] = useState();
+
+    /*function test(){
         if (purchasePrice != null && purchasePrice != "" && dividend !="" && dividend != null){
             return Math.round(parseFloat(dividend)/parseFloat(purchasePrice)*100);
         }
@@ -61,99 +74,157 @@ export default function (){
         else{
             return purchasePrice ? purchasePrice : 0
         }
-    }
+    }*/
 
     const HandleAddProduct = (values) => {
 
         const addRequest = AddProductRequest;
 
-        addRequest.name = values.name;
+       /* addRequest.name = values.name;
         addRequest.barcode = values.barcode.toString();
         addRequest.dividend = dividend;
         addRequest.purchasePrice = purchasePrice;
         addRequest.categoryName = values.categoryName;
         addRequest.description = values.description;
         addRequest.quantity = values.quantity;
-        addRequest.price = calcPrice();
+        addRequest.price = calcPrice();*/
 
         axios.post(API_ADD_PRODUCT,addRequest)
             .then((res) => {
-                console.log(res.data)
             })
             .err((res) => {
-                console.log(res.message)
             })
+    }
+
+    function CheckBarcode(_barcode){
+        setBarcodeError("");
+        console.log(_barcode,_barcode.length)
+        if (_barcode.length == 13){
+            setBarcode(_barcode)
+            setBarcodeError("Barkod kontrol ediliyor lütfen bekleyiniz");
+        }
+        else if(_barcode.length >13 || _barcode.length < 13){
+            setBarcodeError("Lütfen geçerli bir barkod giriniz");
+        }
+    }
+
+    function CalcPurchasePrice(_purchasePrice){
+        setPurchasePriceError("");
+        setPurchasePrice(_purchasePrice)
+        if (_purchasePrice != null && _purchasePrice >= 1){
+        }
+        else{
+            setPurchasePriceError("Lütfen geçerli bir fiyat giriniz")
+        }
+    }
+
+    function CalcDividend(_dividend){
+        setDividendError("");
+        setDividend(_dividend)
+        if (_dividend != null && _dividend >= 0){
+        }
+        else{
+            setDividendError("Lütfen geçerli bir fiyat giriniz")
+        }
+    }
+
+    function dividendCalc(){
+        if ( purchasePrice != null && purchasePrice >= 1){
+            if (dividend != null && dividend >=1){
+                return Math.round(parseFloat(dividend) / parseFloat(purchasePrice) * 100);
+            }
+            else{
+                return 0;
+            }
+        }
+        else{
+            return 0;
+        }
+    }
+
+    function priceCalc(){
+        if ( purchasePrice != null &&purchasePrice >= 1){
+            if (dividend != null && dividend >=1){
+                return parseFloat(purchasePrice) + parseFloat(dividend);
+            }
+            else{
+                return parseFloat(purchasePrice);
+            }
+        }
+        else{
+            return 0;
+        }
+    }
+
+    function clearForm(){
+        setDividendError("");
+        setDividend(0);
+        setPurchasePriceError("");
+        setPurchasePrice(1);
+        setCategory("");
+        setQuantity(1);
+        setBarcode();
+        setName("");
+        setStatus("");
+        setNameError("");
+        setCategoryError("");
+        setDescriptionError("");
+        setPriceError("");
+        setQuantityError("");
     }
 
     return(
         <div>
-            <Formik initialValues={initialValues} validationSchema={schema} onSubmit={(values) => HandleAddProduct(values)}>
-                <Form>
-                    <div className=" mb-4">
-                        <div className="form-group first rounded-3 bg-white border">
-                            <label htmlFor="">Ürün barkodu </label>
-                            <Field type="number" className="form-control" name="barcode" placeholder="Barkod" ></Field>
-                        </div>
-                        <ErrorMessage name="barcode" render={(errorMessage) => <p className="mt-2 text-danger">{errorMessage}</p>}></ErrorMessage>
-                    </div>
-                    <div className=" mb-4">
-                        <div className="form-group first rounded-3 bg-white border">
-                            <label htmlFor="">Ürün adı </label>
-                            <Field type="text" className="form-control" name="name" placeholder="Ürün adı" ></Field>
-                        </div>
-                        <ErrorMessage name="name" render={(errorMessage) => <p className="mt-2 text-danger">{errorMessage}</p>}></ErrorMessage>
-                    </div>
-                    <div className=" mb-4">
-                        <div className="form-group first rounded-3 bg-white border">
-                            <label htmlFor="">Ürün açıklaması </label>
-                            <Field type="text" className="form-control" name="description" placeholder="Ürün açıklaması" ></Field>
-                        </div>
-                        <ErrorMessage name="description" render={(errorMessage) => <p className="mt-2 text-danger">{errorMessage}</p>}></ErrorMessage>
-                    </div>
-                    <div className=" mb-4">
-                        <div className="form-group first rounded-3 bg-white border">
-                            <label htmlFor="">Ürün kategorisi </label>
+           <p>{status}</p>
+            <form  onSubmit={(e) => {e.preventDefault();HandleAddProduct()}}>
+                <div>
+                    <label htmlFor="barcode">Ürün barkodu</label>
+                    <input type="number" min="1" value={barcode} required onChange={(e) => {CheckBarcode(e.target.value)}}/>
+                    <label htmlFor="">{barcodeError}</label>
+                </div>
+                <div>
+                    <label htmlFor="name">Ürün adı</label>
+                    <input type="text" value={name} required onChange={(e) => {setName(e.target.value)}}/>
+                    <label htmlFor="">{nameError}</label>
+                </div>
+                <div>
+                    <label htmlFor="name">Ürün açıklaması</label>
+                    <input type="text" value={description} required onChange={(e) => {setDescription(e.target.value)}}/>
+                    <label htmlFor="">{descriptionError}</label>
+                </div>
+                <div>
+                    <label htmlFor="name">Ürün Kategorisi</label>
+                    <select name="" id="" value={category} onChange={(e) => {setCategory(e.target.value)}}>
+                        {
+                            categories.map((item,key) => {
+                                return <option value={item.name} key={key}>{item.name}</option>
+                            })
+                        }
+                    </select>
+                    <label htmlFor="">{categoryError}</label>
+                </div>
+                <div>
+                    <label htmlFor="name">Ürün Adedi</label>
+                    <input type="number" value={quantity} min="1" required onChange={(e) => {setQuantity(e.target.value)}}/>
+                    <label htmlFor="">{quantityError}</label>
+                </div>
+                <div>
+                    <label htmlFor="name">Ürün Geliş Fiyatı</label>
+                    <input type="number" value={purchasePrice} min="1" required onChange={(e) => {CalcPurchasePrice(e.target.value)}}/>
+                    <label htmlFor="">{purchasePriceError}</label>
+                </div>
+                <div>
+                    <label htmlFor="name">Ürün Kar payı tl</label>
+                    <input type="number" value={dividend} min="0" required onChange={(e) => {CalcDividend(e.target.value)}}/>
+                    <label htmlFor="">{dividendError}</label>
+                </div>
+                <p>Ürün kar payı % <span>{dividendCalc()}</span></p>
+                <p>Ürün satış fiyatı tl <span>{priceCalc()}</span></p>
+                <button onClick={() =>{clearForm()}}>Temizle</button>
+                <button>Ürünü ekle</button>
+            </form>
 
-                            <Field as="select" className="form-control" name="categoryName" placeholder="Ürün adı" >
-                                {categories.map((item,key) => {
-                                    return <option value={item.name} key={key}>{item.name}</option>
-                                })}
-                            </Field>
-                        </div>
-                        <ErrorMessage name="categoryName" render={(errorMessage) => <p className="mt-2 text-danger">{errorMessage}</p>}></ErrorMessage>
-                    </div>
-                    <div className=" mb-4">
-                        <div className="form-group first rounded-3 bg-white border">
-                            <label htmlFor="">Ürün adedi </label>
 
-                            <Field type="number" min="1" className="form-control" name="quantity" placeholder="Ürün adedi" ></Field>
-                        </div>
-                        <ErrorMessage name="quantity" render={(errorMessage) => <p className="mt-2 text-danger">{errorMessage}</p>}></ErrorMessage>
-                    </div>
-                    <div className=" mb-4">
-                        <div className="form-group first rounded-3 bg-white border">
-                            <label htmlFor="">Ürün geliş fiyatı </label>
-
-                            <Field type="number" min="1" className="form-control" name="purchasePrice" value={purchasePrice} onChange={(e) => {setPurchasePrice(e.target.value)}} placeholder="Ürün adedi" ></Field>
-                        </div>
-                        <ErrorMessage name="purchasePrice" render={(errorMessage) => <p className="mt-2 text-danger">{errorMessage}</p>}></ErrorMessage>
-                    </div>
-                    <div className=" mb-4">
-                        <div className="form-group first rounded-3 bg-white border">
-                            <label htmlFor="">Ürün kar payı tl </label>
-
-                            <Field type="number" min="0" className="form-control" name="dividend" value={dividend} onChange={(e) => {setDividend(e.target.value)}} placeholder="Ürün kar payı" ></Field>
-                        </div>
-                        <ErrorMessage name="dividend" render={(errorMessage) => <p className="mt-2 text-danger">{errorMessage}</p>}></ErrorMessage>
-                    </div>
-                    <div>
-                        <p>Ürün karı % : <span>{test()}</span></p>
-                        <p>Satış fiyatı : <span>{calcPrice()}</span></p>
-                        <button type="submit" color="info">Ürünü ekle</button>
-                    </div>
-                    <p className="text-muted mt-3"><Link to="/forgot-password">Şifremi unuttum</Link></p>
-                </Form>
-            </Formik>
         </div>
     )
 }
